445275185d5b29bb0ef83b8ab9873813f1a6e7aa
Taras Danyliuk <tarik.danylyuk@gmail.com>
2016-09-20 13:52:51 +0300

    chat

--- chat-server.js
+++ chat-server.js
@@ -1,9 +1,5 @@
-// chat-server.js
-
-// http://ejohn.org/blog/ecmascript-5-strict-mode-json-and-more/
 "use strict";
 
-// Optional. You will see this name in eg. 'ps' or 'top' command
 process.title = 'node-chat';
 
 // Port where we'll run the websocket server
@@ -13,13 +9,10 @@ var webSocketsServerPort = 1337;
 var webSocketServer = require('websocket').server;
 var http = require('http');
 
-/**
- * Global variables
- */
-// entire message history
 var history = new Array();
-// list of currently connected clients (users)
 var clients = new Array();
+var arr_clients = [];
+var arr_topics = [];
 
 // Array with some colors
 var colors = [ 'red', 'green', 'blue', 'magenta', 'purple', 'plum', 'orange' ];
@@ -32,6 +25,8 @@ colors.sort(function(a,b) { return Math.random() > 0.5; } );
 var server = http.createServer(function(request, response) {
     // Not important for us. We're writing WebSocket server, not HTTP server
 });
+
+
 server.listen(webSocketsServerPort, function() {
     console.log((new Date()) + " Server is listening on port " + webSocketsServerPort);
 });
@@ -49,53 +44,85 @@ wsServer.on('request', function(request) {
     console.log((new Date()) + ' Connection from origin ' + request.origin + '.');
 
     // accept connection
-    var connection = request.accept(null, request.origin); 
+    var connection = request.accept(null, request.origin);
+
     // we need to know client index to remove them on 'close' event
     var index = clients.push(connection) - 1;
     var userName = false;
     var userColor = false;
+    var topic = false;
 
     console.log((new Date()) + ' Connection accepted.');
 
     // send back chat history
-    if (history.length > 0) {
-        connection.sendUTF(JSON.stringify( { type: 'history', data: history} ));
+    //if (history.length > 0) { 
+    //    connection.sendUTF(JSON.stringify( { type: 'history', data: history} ));
+    //}
+    if (arr_topics.length > 0) { 
+        connection.sendUTF(JSON.stringify( { type: 'topics', data: arr_topics} ));
     }
 
     // user sent some message
     connection.on('message', function(message) {
         if (message.type === 'utf8') { // accept only text
             if (userName === false) { // first message sent by user is their name
+
                 // remember user name
-                userName = message.utf8Data;
+                var msg = message.utf8Data
+                userName = (msg.split(','))[0]; //receive data from user
+                topic = (msg.split(','))[1];
+
+                arr_clients.push(userName);
+                if ( arr_topics.indexOf(topic) < 0 ) arr_topics.push(topic);
+
                 // get random color and send it back to the user
                 userColor = colors.shift();
+
                 connection.sendUTF(JSON.stringify({ type:'color', data: userColor }));
                 console.log((new Date()) + ' User is known as: ' + userName
                             + ' with ' + userColor + ' color.');
 
+                if (history.length > 0) { 
+                    connection.sendUTF(JSON.stringify( { type: 'history', data: history} ));
+                };
+
+                if (arr_clients.length > 0) { 
+                   connection.sendUTF(JSON.stringify({ type: 'clients', data: arr_clients} ));
+                }
+
+
             } else { // log and broadcast the message
-                console.log((new Date()) + ' Received Message from '
-                            + userName + ': ' + message.utf8Data);
+                console.log((new Date()) + ' Received Message from ' + userName + ': ' + message.utf8Data);
 
                 // we want to keep history of all sent messages
                 var obj = {
                     time: (new Date()).getTime(),
-                    text: message.utf8Data,
+                    text: (message.utf8Data).substring(0, (message.utf8Data).length - 1),
                     author: userName,
-                    color: userColor//,
-                   // topic: topic.utf8Data
+                    color: userColor,
+                    topic: topic
                 };
+                
                 history.push(obj);
 
-                // broadcast message to all connected clients
+                // send message to all connected clients
                 var json = JSON.stringify({ type:'message', data: obj });
                 for (var i=0; i < clients.length; i++) {
                     clients[i].sendUTF(json);
+                };
+
+                if (arr_topics.length > 0) {
+                   connection.sendUTF(JSON.stringify({ type: 'topics', data: arr_topics} ));
+                }
+
+                if (arr_clients.length > 0) { console.log('clients - ' + arr_clients);
+                   connection.sendUTF(JSON.stringify({ type: 'clients', data: arr_clients} ));
                 }
             }
         }
     });
+
+
 
     // user disconnected
     connection.on('close', function(connection) {
@@ -103,6 +130,7 @@ wsServer.on('request', function(request) {
             console.log((new Date()) + " Peer " + connection.remoteAddress + " disconnected.");
             // remove user from the list of connected clients
             clients.splice(index, 1);
+            arr_clients.splice(index, 1);
             // push back user's color to be reused by another user
             colors.push(userColor);
         }
--- frontend-angular.js
+++ frontend-angular.js
@@ -6,7 +6,12 @@ angular.module("appChat", [])
     var content = document.getElementById('content');
     var input = document.getElementById('input');
     var status = document.getElementById('status');
+    var topic = document.getElementById('topic');
+    var topicName = document.getElementById('topic-name');
+
     $scope.users = [];
+    $scope.topics = [];
+    var my_topic;
     // my color assigned by the server
     var myColor = false;
     // my name sent to the server
@@ -37,6 +42,14 @@ angular.module("appChat", [])
         content.html('Sorry, but there\'s some problem with your ' + 'connection or the server is down.</p>' );
     };
 
+    connection.onclose = function(event) {
+            if (event.wasClean) { alert('Connect closed.');
+            } else {
+              alert('Обрыв соединения'); // например, "убит" процесс сервера
+              }
+            alert('Code: ' + event.code + ' reason: ' + event.reason);
+        };
+
     // most important part - incoming messages
     connection.onmessage = function (message) {
         // try to parse JSON message. Because we know that the server always returns
@@ -61,61 +74,64 @@ angular.module("appChat", [])
         } else if (json.type === 'history') { // entire message history
             // insert every single message to the chat window
             for (var i=0; i < json.data.length; i++) {
-                if (json.data[i].author) {
-                    if ($scope.users.indexOf(json.data[i].author) < 0) {
-                    $scope.users.push(json.data[i].author); console.log('$scope.users - ' + $scope.users); } }
-                addMessage(json.data[i].author, json.data[i].text,
-                           json.data[i].color, new Date(json.data[i].time));
+                //if (json.data[i].author) {
+                    //if ($scope.users.indexOf(json.data[i].author) < 0) {
+                    //$scope.users.push(json.data[i].author); console.log('$scope.users - ' + $scope.users); } }
+                if (json.data[i].topic == my_topic)
+                    addMessage(json.data[i].author, json.data[i].text, json.data[i].color, new Date(json.data[i].time));
             }
             slideScrollbar();
-        } else if (json.type === 'message') { // it's a single messagegg
+        }
+        
+        else if (json.type === 'clients') { $scope.users = json.data; }
+        else if (json.type === 'topics') { $scope.topics = json.data; }
+
+        else if (json.type === 'message') { // it's a single messagegg
             input.removeAttribute('disabled'); // let the user write another message
-            if (json.data.author) {
-                    if ($scope.users.indexOf(json.data.author) < 0) {
-                    $scope.users.push(json.data.author); console.log('$scope.users - ' + $scope.users); } }
-            addMessage(json.data.author, json.data.text,
-                       json.data.color, new Date(json.data.time) );
+            console.log('json.data.topic: ' + json.data.topic + ',  my_topic:' + my_topic);
+            if (json.data.topic == my_topic) addMessage(json.data.author, json.data.text, json.data.color, new Date(json.data.time) );
             slideScrollbar();
         } else {
             console.log('Hmm..., I\'ve never seen JSON like this: ', json);
         }
     };
 
+
+
     $scope.keydown = function(e) {
-        if (e.keyCode === 13) {
-            var msg = input.value;
-            connection.send(msg);
+        if (e.keyCode === 13 ) {
+            var msg = input.value + ',' + topic.value;
+
+
+            connection.send(msg);  //send data to server
+            
             input.value = '';
             input.setAttribute('disabled', 'disabled');
 
             if (myName === false) {
-                myName = msg;
-                if (myName) {
-                    if ($scope.users.indexOf(myName) < 0) {
-                    $scope.users.push(myName); console.log('$scope.users - ' + $scope.users); } }
+                myName = msg.split(',')[0];
+                my_topic = topic.value;
+                topicName.innerHTML = 'Your topic: ' + my_topic;
+                topic.value = '';
+                topic.setAttribute('disabled', 'disabled');
+
+                //if (myName) {
+                    //if ($scope.users.indexOf(myName) < 0) {
+                    //$scope.users.push(myName); console.log('$scope.users - ' + $scope.users); } }
                 console.log('First time - ' + myName);
             }
-        }
+        } //else alert('Enter topic name');
     };
 
     setInterval(function() {
         if (connection.readyState !== 1) {
-            status.innerHTML('Error');
-            input.attr('disabled', 'disabled').val('Unable to comminucate '
-                                                 + 'with the WebSocket server.');
+            status.innerHTML = 'Error';
+            input.setAttribute('disabled', 'disabled');
+            input.value = 'Unable to comminucate with the WebSocket server.';
         }
     }, 5000);
 
-    $scope.disconnect = function() {
-        connection.onclose = function(event) {
-            if (event.wasClean) {
-            alert('Соединение закрыто чисто');
-            } else {
-            alert('Обрыв соединения'); // например, "убит" процесс сервера
-            }
-            alert('Код: ' + event.code + ' причина: ' + event.reason);
-        };
-    };
+    $scope.disconnect = function() { location.reload();   };
 
 
     function addMessage(author, message, color, datetime) {
@@ -129,7 +145,9 @@ angular.module("appChat", [])
     
     
 
-    var scrollbar = $('body > section:first').tinyscrollbar();
+
+
+    var scrollbar = $('body > div:first').tinyscrollbar();
     
     function slideScrollbar() {
         scrollbar.update();
